b8a49c90c99d0cc42515480b718127d4cbd77d87
Samuel de Leon <sapdeleon@msn.com>
2024-12-25 12:54:52 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+// HINTs: Use the axios documentation as well as the video lesson to help you.
+// https://axios-http.com/docs/post_example
+// Use the Secrets API documentation to figure out what each route expects and how to work with it.
+// https://secrets-api.appbrewery.com/
+
+//TODO 1: Add your own bearer token and configuration from the previous lesson.
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "Waiting for data..." });
+});
+
+app.post("/get-secret", async (req, res) => {
+
+});
+
+app.post("/post-secret", async (req, res) => {
+  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+});
+
+app.post("/put-secret", async (req, res) => {
+  const searchId = req.body.id;
+  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+});
+
+app.post("/patch-secret", async (req, res) => {
+  const searchId = req.body.id;
+  // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+});
+
+app.post("/delete-secret", async (req, res) => {
+  const searchId = req.body.id;
+  // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
